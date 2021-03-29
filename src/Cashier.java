78fcb0a990525bf07ded5296962242285ec9255a
yas003 <52592587+yasamankfd@users.noreply.github.com>
2021-03-30 00:16:27 +0430

    first edit

---
+++ src/Cashier.java
@@ -0,0 +1,7 @@
+public class Cashier extends Employee {
+
+    public Cashier(String name,String lastname,String address,String Nid,String phoneNumber,String emailAddress,int age,boolean gender)
+    {
+        super(name,lastname,address,Nid,phoneNumber,emailAddress,age,gender);
+    }
+}
--- src/Janitor.java
+++ src/Janitor.java
@@ -1,6 +1,6 @@
 public class Janitor extends Employee{
 
-    public Janitor(String name,String lastname,String address,String Nid,String phoneNumber,String emailAddress,int age,boolean gender,String tailoringCertificateCode)
+    public Janitor(String name,String lastname,String address,String Nid,String phoneNumber,String emailAddress,int age,boolean gender)
     {
         super(name,lastname,address,Nid,phoneNumber,emailAddress,age,gender);
     }
